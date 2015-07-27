9362e9cb7ac22bdfb148419d6d842f6777db7de2
Nikola Radmanovic <Radmanovic@users.noreply.github.com>
2015-07-27 10:39:33 +0200

    Update and rename prosek.py to prosek.pas

---
+++ prosek.pas
@@ -0,0 +1,58 @@
+{##########################################}
+{#           Nikola Radmanovic            #}
+{#      http://www.github.com/Radmanovic  #}
+{#                2015                    #}
+{##########################################}
+Program Prosek(output);
+var a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, s, P:Real;
+begin
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(a);
+  
+  writeln('Unesite ocenu iz maternjeg jezika ');
+  readln(b);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(c);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(d);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(e);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(f);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(g);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(h);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(i);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(j);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(k);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(l);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(m);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(n);
+  
+  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  readln(o);
+    
+  s:=(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o); 
+  P:=s/15;
+  writeln('Vas prosek iznosi',P); 
+  readln;
+end.
