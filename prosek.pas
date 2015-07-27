9fa81bd26ebbc6cefff3580a97d69c7d3c372a5c
Nikola Radmanovic <Radmanovic@users.noreply.github.com>
2015-07-28 00:31:47 +0200

    Update prosek.pas

--- prosek.pas
+++ prosek.pas
@@ -12,58 +12,61 @@ begin
   writeln('Unesite ime i prezime ucenika:');
   readln(ime, prezime);
   {Predmeti}
-  writeln('Unesite vasu ocenu iz maternjeg jezika');
+  writeln('Unesite ocenu iz maternjeg jezika');
   readln(a);
   
   writeln('Unesite ocenu iz I stranog jezika');
   readln(b);
   
-  writeln('Unesite vasu ocenu iz II stranog jezika');
+  writeln('Unesite ocenu iz II stranog jezika');
   readln(c);
   
-  writeln('Unesite vasu ocenu iz fizike');
+  writeln('Unesite ocenu iz fizike');
   readln(d);
   
-  writeln('Unesite vasu ocenu iz geografije');
+  writeln('Unesite ocenu iz geografije');
   readln(e);
   
-  writeln('Unesite vasu ocenu iz hemije');
+  writeln('Unesite ocenu iz hemije');
   readln(f);
   
-  writeln('Unesite vasu ocenu iz biologije');
+  writeln('Unesite ocenu iz biologije');
   readln(g);
   
-  writeln('Unesite vasu ocenu iz matematike');
+  writeln('Unesite ocenu iz matematike');
   readln(h);
   
-  writeln('Unesite vasu ocenu iz istorije');
+  writeln('Unesite ocenu iz istorije');
   readln(i);
   
-  writeln('Unesite vasu ocenu iz latinskog jezika');
+  writeln('Unesite ocenu iz latinskog jezika');
   readln(j);
   
-  writeln('Unesite vasu ocenu iz psihologije');
+  writeln('Unesite ocenu iz psihologije');
   readln(k);
   
-  writeln('Unesite vasu ocenu iz likovne kulture');
+  writeln('Unesite ocenu iz likovne kulture');
   readln(l);
   
-  writeln('Unesite vasu ocenu iz muziicke kulture');
+  writeln('Unesite ocenu iz muziicke kulture');
   readln(m);
   
-  writeln('Unesite vasu ocenu iz fizickog vaspitanja');
+  writeln('Unesite ocenu iz fizickog vaspitanja');
   readln(n);
   
-  writeln('Unesite vasu ocenu iz vladanja');
+  writeln('Unesite ocenu iz vladanja');
   readln(o);
-      
+  {Matematicke operacije}  
   s:=(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o); {Sabira sve ocena}
   P:=s/15; {Vrsi sracunavanje aritmeticke vrednosti}
+  
   {Kraj}
   write('Ime ucenika: ',ime,' ',prezime);
   read;
   writeln('Prosek: ',P); {Ispisuje aritmeticku vrednost}
   readln;
+ 
   write('Trenutno koristite verziju 0.0.1'); 
+  readln;
   write('Hvala sto ste koristili program source code se nalazi na http://www.github.com/Radmanovic/Prosek');
 end.
