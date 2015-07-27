8741f0e13db108094e73f33cd1c37fa288d8e861
Nikola Radmanovic <Radmanovic@users.noreply.github.com>
2015-07-27 18:15:57 +0200

    Update prosek.pas

--- prosek.pas
+++ prosek.pas
@@ -6,46 +6,46 @@
 Program Prosek(output);
 var a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, s, poruka, P:Real;
 begin
-  writeln('Unesite vasu ocenu iz maternjeg jezika ');
+  writeln('Unesite vasu ocenu iz maternjeg jezika');
   readln(a);
   
-  writeln('Unesite ocenu iz I stranog jezika ');
+  writeln('Unesite ocenu iz I stranog jezika');
   readln(b);
   
-  writeln('Unesite vasu ocenu iz II stranog jezika ');
+  writeln('Unesite vasu ocenu iz II stranog jezika');
   readln(c);
   
-  writeln('Unesite vasu ocenu iz fizike ');
+  writeln('Unesite vasu ocenu iz fizike');
   readln(d);
   
-  writeln('Unesite vasu ocenu iz geografije ');
+  writeln('Unesite vasu ocenu iz geografije');
   readln(e);
   
-  writeln('Unesite vasu ocenu iz hemije ');
+  writeln('Unesite vasu ocenu iz hemije');
   readln(f);
   
-  writeln('Unesite vasu ocenu iz biologije ');
+  writeln('Unesite vasu ocenu iz biologije');
   readln(g);
   
-  writeln('Unesite vasu ocenu iz matematike ');
+  writeln('Unesite vasu ocenu iz matematike');
   readln(h);
   
-  writeln('Unesite vasu ocenu iz istorije ');
+  writeln('Unesite vasu ocenu iz istorije');
   readln(i);
   
-  writeln('Unesite vasu ocenu iz latinskog jezika ');
+  writeln('Unesite vasu ocenu iz latinskog jezika');
   readln(j);
   
-  writeln('Unesite vasu ocenu iz psihologije ');
+  writeln('Unesite vasu ocenu iz psihologije');
   readln(k);
   
-  writeln('Unesite vasu ocenu iz likovne kulture ');
+  writeln('Unesite vasu ocenu iz likovne kulture');
   readln(l);
   
-  writeln('Unesite vasu ocenu iz muziicke kulture ');
+  writeln('Unesite vasu ocenu iz muziicke kulture');
   readln(m);
   
-  writeln('Unesite vasu ocenu iz fizickog vaspitanja ');
+  writeln('Unesite vasu ocenu iz fizickog vaspitanja');
   readln(n);
   
   writeln('Unesite vasu ocenu iz vladanja');
