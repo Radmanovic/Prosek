97bc5b91c2e692ce2c11a63d02f6f9c1addb70dc
Nikola Radmanovic <radmanovic@users.noreply.github.com>
2015-11-25 12:47:44 +0100

    Python
    Added current progress of the python version of the program

---
+++ prosek.py
@@ -0,0 +1,57 @@
+##########################################
+#           Nikola Radmanovic            #
+#      http://www.github.com/radmanovic  #
+#                2015                    #
+##########################################
+
+#Skole
+
+skole = ["Gimnazije", "Strucne skole", "Umetnicke skole"]
+
+#/Skole#
+
+#Smerovi
+
+skole[0] = ["Prirodno matematicki", "Drustveno-jezicki", "Opsti"]
+
+skole[1] = ["Komercijalista", "Turisticki tehnicar", "Masinski tehnicar"]
+
+#/Smerovi#
+
+#Predmeti
+
+#Gimnazije predmeti
+
+skole[0][0] = ["Matematika",
+               "Srpski jezik",
+               "Hemija",
+               "Strani jezik",
+               "Engleski"]
+
+skole[0][1] = ["Matematika",
+               "Srpski jezik",
+               "Hemija",
+               "Strani jezik",
+               "Engleski",
+               "Latinski jezik"]
+
+skole[0][2] = ["Matematika",
+               "Srpski jezik",
+               "Hemija",
+               "Strani jezik",
+               "Engleski"]
+
+#/Gimnazije predmeti#
+
+#/Predmeti#
+
+#Izborni predmeti
+izborni = ["Verska nastava", "Gradjanska nastava"]
+#/Izborni predmeti#
+
+#Funkcije
+skole[0][0][0] = input("Unesite ocenu iz matematike")
+print(skole[0][0][0])
+
+#/Funkcije#
+# Repository location https://github.com/radmanovic/prosek #
